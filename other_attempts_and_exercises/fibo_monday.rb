9a3638566cd7c71c659ac81b47c2969c0d473d9d
Manuel <salomanuel.aux+code@gmail.com>
2016-10-17 17:26:16 +0200

    fibo RECURSION SIMPLE WITH LIST FREAKING WORKS

--- other_attempts_and_exercises/fibo_monday.rb
+++ other_attempts_and_exercises/fibo_monday.rb
@@ -5,7 +5,6 @@ def fibs(n)
 	return n if n <= 1
 	return fibs(n-1) + fibs(n-2)
 end
-
 # 0.upto(10) { |i| puts fibs(i) }
 
 def i_fibs(n)
@@ -13,5 +12,12 @@ def i_fibs(n)
 	list << list[-2] + list[-1] while list.length < n
 	return list
 end
+# puts i_fibs(10).join(", ")
 
-puts i_fibs(10).join(", ")
+def fibs_list(n,list=[])
+	return list if list.length >= n
+	fibs_list(n-1, list)
+	list << fibs(n-1)
+end
+
+puts fibs_list(10).join(", ")
