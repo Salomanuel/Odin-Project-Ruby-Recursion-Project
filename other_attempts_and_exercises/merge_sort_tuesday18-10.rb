3f003bfd59761028ff8a44a9b1ea4f2501273f64
Manuel <salomanuel.aux+code@gmail.com>
2016-10-19 13:03:08 +0200

    mergeSort now works with even AND EVEN ODD numbers!

--- other_attempts_and_exercises/merge_sort_tuesday18-10.rb
+++ other_attempts_and_exercises/merge_sort_tuesday18-10.rb
@@ -1,3 +1,9 @@
+def rando(qty)
+	list = qty.times.map { rand(qty * 1.5)+1 }
+end
+
+# puts rando(20).join(", ")
+
 def tinyMerge(ary1,ary2,aryF=[])
 	while ary1.length > 0 and ary2.length > 0
 		aryF << ((ary1[0] <= ary2[0])? ary1.shift : ary2.shift)
@@ -11,29 +17,23 @@ end
 # puts tinyMerge([3,4],[1,2])
 # puts tinyMerge([3, 7, 9, 11],[2, 6, 12, 14])
 
-
 def mergeSort(ary)
-	
-	mid = ary.length/2
-	a   = ary[0...mid]
-	b   = ary[mid..-1]
-
-	if ary.length >= 3
-		left  = mergeSort(a).flatten
-		right = mergeSort(b).flatten
+	if ary.length >= 2
+		left  = mergeSort(ary[0...(ary.length/2)]).flatten
+		right = mergeSort(ary[(ary.length/2)..-1]).flatten
 		# puts "#{left} : #{right}" 	 if left != nil and right != nil
 		return tinyMerge(left,right) if left != nil and right != nil
 	else
+		return ary if ary.length == 1
 		ary = ary[1],ary[0] if ary[0] > ary[1]
 		return ary
 	end
-
-	# puts "#{ary}"
-	# puts mergeSort(a) + mergeSort(b)
 end
 
-
-ary = [14, 7, 3, 12, 9, 11, 6, 2]
-# ary = [14,7,3,12]
-
-puts mergeSort(ary).join(", ")
+ary1 = [14,7,3,12]
+ary2 = [14, 7, 3, 12, 9, 11, 6, 2]
+# puts mergeSort(ary2).join(", ")
+nums = rando(347)
+puts nums.join(", ")
+puts
+puts mergeSort(nums).join(", ")
